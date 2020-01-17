936c4a3206acffacbe252272dbdd1b29c8fd36ee
aysttu <36916587+aysttu@users.noreply.github.com>
2020-01-17 08:04:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,14 @@
-function openCity(cityName, elmnt, color) {
-  // Hide all elements with class="tabcontent" by default */
+function openPage(pageName,elmnt,color) {
   var i, tabcontent, tablinks;
-
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
-
-  // Remove the background color of all tablinks/buttons
-  //tablinks = document.getElementsByClassName("tablink");
-  //for (i = 0; i < tablinks.length; i++) {
-  //  tablinks[i].style.backgroundColor = "";
-  //}
-
-  // Show the specific tab content
-  document.getElementById(cityName).style.display = "block";
-
-  // Add the specific color to the button used to open the tab content
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+  }
+  document.getElementById(pageName).style.display = "block";
   elmnt.style.backgroundColor = color;
 }
 
